a31665731ad6d98d4a15b6e5cdf36d1099c5bae0
sherr04 <161237156+sherr04@users.noreply.github.com>
2025-10-16 15:43:25 +0800

    modified html

--- vg_1.js
+++ vg_1.js
@@ -9,3 +9,15 @@ var vg_2 = "bar.vg.json";
 vegaEmbed("#bar", vg_2).then(function(result) {
     // Access the Vega view instance as result.view
 }).catch(console.error);
+
+var vg_3 = "bar2.vg.json";
+
+vegaEmbed("#stackbar", vg_3).then(function(result) {
+    // Access the Vega view instance as result.view
+}).catch(console.error);
+
+var vg_4 = "heat.vg.json";
+
+vegaEmbed("#heatmap", vg_4).then(function(result) {
+    // Access the Vega view instance as result.view
+}).catch(console.error);
